f6fe9e0420fbd77fa2cec303d0c74f5b6487053d
MiaLe98 <mia.le0711@gmail.com>
2020-10-28 09:28:14 +0200

    deploy to heroku

--- app.py
+++ app.py
@@ -3,52 +3,30 @@ from flask import Flask,render_template,url_for,request
 import pandas as pd 
 import joblib
 import nltk
-from flask_mysqldb import MySQL
+import numpy as np
 
-#per risolvere un bug, altrimenti da errore
 nltk.download('punkt')
-
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'minhanh0711'
-app.config['MYSQL_DB'] = 'MyDB'
 
-mysql = MySQL(app)
-
-@app.route('/')
-def home():
-	return render_template('home.html')
-
-@app.route('/predict',methods=['GET', 'POST'])
+@app.route('/',methods=['POST'])
 def predict():
 
-    classifier = joblib.load("ml-model.cls")
-    
-    if request.method == 'POST':
-        def format_sentence(sent):
-            return({word: True for word in nltk.word_tokenize(sent)})
+    classifier = joblib.load("model-73acc.cls")
 
-        message = request.form['message']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO MyDB(message) VALUES (%s)", ([message]))
-        mysql.connection.commit()
-        cur.close()
-        my_prediction = classifier.classify(format_sentence(message))
-    return render_template('result.html',prediction = my_prediction)
+    def format_sentence(sent):
+        return np.array([sent])
 
-@app.route('/opinion',methods=['GET', 'POST'])
-def opinion(): 
-    if request.method == 'POST': 
-        req = request.form['options'] 
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO MyDB(opinion) VALUES (%s)", ([req]))
-        mysql.connection.commit()
-        cur.close()
-        print(req)
-        return render_template('home.html')
-
-    return render_template('opinion.html', opinion = req)
+    data = request.get_json(force=True)
+    message = str(data)
+    prediction = classifier.predict(format_sentence(message))
+    if prediction == 'pos': 
+        ans = 'Positive'
+    elif prediction == 'neg': 
+        ans = 'Negative'
+    elif prediction == 'neu': 
+        ans = 'Neutral'
+    output = {'results': ans}
+    return output
 
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0", port = 80, debug=True)
+    app.run(port = 5000, debug=True)
