bcd4f2063755e393d6641d41d2ae681e0f5854f3
MiaLe98 <mia.le0711@gmail.com>
2020-11-02 09:49:02 +0200

    Update files for deployment

--- app.py
+++ app.py
@@ -4,28 +4,30 @@ import pandas as pd
 import joblib
 import nltk
 import numpy as np
+import os 
 
 nltk.download('punkt')
 app = Flask(__name__)
 
 @app.route('/',methods=['POST'])
 def predict():
+    filename = os.path.join(app.config['APP_PATH'], 'model-73acc.cls')
+    with open(filename, 'rb') as f:
+        classifier = joblib.load(f)
 
-    classifier = joblib.load("model-73acc.cls")
+        def format_sentence(sent):
+            return np.array([sent])
 
-    def format_sentence(sent):
-        return np.array([sent])
-
-    data = request.get_json(force=True)
-    message = str(data)
-    prediction = classifier.predict(format_sentence(message))
-    if prediction == 'pos': 
-        ans = 'Positive'
-    elif prediction == 'neg': 
-        ans = 'Negative'
-    elif prediction == 'neu': 
-        ans = 'Neutral'
-    output = {'results': ans}
+        data = request.get_json(force=True)
+        message = str(data)
+        prediction = classifier.predict(format_sentence(message))
+        if prediction == 'pos': 
+            ans = 'Positive'
+        elif prediction == 'neg': 
+            ans = 'Negative'
+        elif prediction == 'neu': 
+            ans = 'Neutral'
+        output = {'results': ans}
     return output
 
 if __name__ == '__main__':
--- result.py
+++ result.py
@@ -1,8 +1,9 @@
 import requests
 import json 
 
-url = 'http://127.0.0.1:5000/'
-data = {'data': 'Jag hatar dig'}
+url = 'https://sv-sentimentanalysis.herokuapp.com/'
+#url = 'http://127.0.0.1:5000/'
+data = {'data': "Jag hatar dig"}
 data = json.dumps(data)
 send = requests.post(url, data)
 ans=send.json()
